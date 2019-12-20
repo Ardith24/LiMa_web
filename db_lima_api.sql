ff7a8168823c3555da10a0dbaae0146c38a79f7e
Ardith Lutfiawan <ardith.lutfiawan@gmail.com>
2019-12-20 21:15:16 +0700

    siap grak

--- db_lima_api.sql
+++ db_lima_api.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 30, 2019 at 04:26 PM
+-- Generation Time: Dec 15, 2019 at 04:14 PM
 -- Server version: 10.1.41-MariaDB-0+deb9u1
--- PHP Version: 7.0.33-0+deb9u5
+-- PHP Version: 7.0.33-13+0~20191128.24+debian9~1.gbp832d85
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -61,16 +61,16 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_10_12_215750_create_sprints_table', 1),
 (7, '2019_10_15_065716_create_kontaks_table', 2),
 (8, '2019_10_15_090506_create_tasks_table', 3),
 (9, '2019_10_19_013824_create_kejars_table', 4),
 (10, '2019_10_20_223521_create_sprints_table', 5),
-(11, '2019_10_21_001148_create_sprints_table', 6),
-(12, '2019_10_22_082727_create_kesulitans_table', 7),
-(15, '2019_10_22_083704_create_tasks_table', 8);
+(21, '2014_10_12_000000_create_users_table', 6),
+(22, '2014_10_12_100000_create_password_resets_table', 6),
+(23, '2019_10_21_001148_create_sprints_table', 6),
+(24, '2019_10_22_082727_create_kesulitans_table', 6),
+(25, '2019_10_22_083704_create_tasks_table', 6);
 
 -- --------------------------------------------------------
 
@@ -79,8 +79,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -93,7 +93,7 @@ CREATE TABLE `password_resets` (
 CREATE TABLE `sprints` (
   `id` int(10) UNSIGNED NOT NULL,
   `nama_sprint` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `desc_sprint` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `desc_sprint` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tgl_mulai` date NOT NULL,
   `tgl_selesai` date NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -106,9 +106,8 @@ CREATE TABLE `sprints` (
 --
 
 INSERT INTO `sprints` (`id`, `nama_sprint`, `desc_sprint`, `tgl_mulai`, `tgl_selesai`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Link n Match', 'KeEnam', '2019-10-26', '2019-10-31', '2019-10-21 15:38:28', '2019-10-25 16:14:51', NULL),
-(11, 'TOKOMEDIA', 'nice', '2019-10-25', '2019-10-27', '2019-10-24 02:23:22', '2019-10-24 02:23:22', NULL),
-(12, 'Jangan Takut Gagal', 'bismillah', '2019-10-26', '2019-10-30', '2019-10-25 00:26:42', '2019-10-25 00:26:42', NULL);
+(3, 'ABCDE', 'Okeee', '2019-12-11', '2019-12-24', '2019-12-03 00:02:08', '2019-12-03 00:02:08', NULL),
+(4, 'Oke Gan', 'Jenis makanan', '2019-12-25', '2019-12-31', '2019-12-03 16:17:55', '2019-12-10 03:52:09', NULL);
 
 -- --------------------------------------------------------
 
@@ -121,7 +120,7 @@ CREATE TABLE `tasks` (
   `sprint_id` int(10) UNSIGNED NOT NULL,
   `nama_task` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `kesulitan_id` int(10) UNSIGNED NOT NULL,
-  `status` tinyint(4) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -131,10 +130,10 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `sprint_id`, `nama_task`, `kesulitan_id`, `status`, `created_at`, `updated_at`) VALUES
-(5, 11, 'Blablaba', 3, 2, '2019-10-25 00:02:10', '2019-10-25 18:06:00'),
-(6, 1, 'Okelah', 2, 1, '2019-10-25 00:30:36', '2019-10-25 00:30:36'),
-(7, 12, 'I love Indonesia', 2, 1, '2019-10-25 15:52:18', '2019-10-25 16:04:11'),
-(9, 1, 'Blabl', 2, 2, '2019-10-29 00:09:35', '2019-10-29 00:09:35');
+(3, 3, 'Blablaba', 3, 1, '2019-12-03 00:02:22', '2019-12-09 19:30:43'),
+(4, 3, 'Old Task', 3, 0, '2019-12-03 00:30:42', '2019-12-03 00:31:26'),
+(5, 4, 'I love Indonesia', 1, 0, '2019-12-10 06:12:16', '2019-12-10 06:12:26'),
+(6, 4, 'Ardith', 4, 1, '2019-12-10 06:14:55', '2019-12-10 06:15:08');
 
 -- --------------------------------------------------------
 
@@ -144,9 +143,9 @@ INSERT INTO `tasks` (`id`, `sprint_id`, `nama_task`, `kesulitan_id`, `status`, `
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -157,8 +156,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Ardith Lutfiawan', 'ardith.lutfiawan@gmail.com', '$2y$10$s752O.d2aXA6ZJuoR7oYyu1PqWH1XTotxEXddz8dd4RNaoIjq4NYK', 'Dn7iuWmgFm41B1FaCFYwbjzZDyQrjHSzo00Vi4Kx3ZFNMDoYXvNxDuK4aJKU', '2019-10-12 16:07:10', '2019-10-12 16:07:10'),
-(2, 'Jundi', 'alifiafebrina@gmail.com', '$2y$10$069xbLGI4TJSTntH9Tji0e0/RuOifATCCu44RxNyA9xJyHwlNe7NO', 'g65GUTB1afrBufNLCFySOhFIUQ4UYOdn9txoNrKCvzl9b8P2QN1BuM1C2hGx', '2019-10-14 21:42:28', '2019-10-14 21:42:28');
+(1, 'Ardith Lutfiawan', 'ardith.lutfiawan@gmail.com', '$2y$10$UvGhhqg2K2eGNME0D71ihu6wOEuN16O3VoLNOkyH2Xzw8mlpxbPj2', 'aYysJBw9AtEOEXaWQU1IUrPfb9BxvdZZD0H07LSVkxQYrQmst541hzjISPxn', '2019-12-02 23:44:16', '2019-12-02 23:44:16');
 
 --
 -- Indexes for dumped tables
@@ -193,8 +191,8 @@ ALTER TABLE `sprints`
 --
 ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `tasks_kesulitan_id_foreign` (`kesulitan_id`),
-  ADD KEY `sprint_id` (`sprint_id`);
+  ADD KEY `tasks_sprint_id_foreign` (`sprint_id`),
+  ADD KEY `tasks_kesulitan_id_foreign` (`kesulitan_id`);
 
 --
 -- Indexes for table `users`
@@ -216,22 +214,22 @@ ALTER TABLE `kesulitans`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `sprints`
 --
 ALTER TABLE `sprints`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -240,8 +238,8 @@ ALTER TABLE `users`
 -- Constraints for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `sprint_id` FOREIGN KEY (`sprint_id`) REFERENCES `sprints` (`id`),
-  ADD CONSTRAINT `tasks_kesulitan_id_foreign` FOREIGN KEY (`kesulitan_id`) REFERENCES `kesulitans` (`id`);
+  ADD CONSTRAINT `tasks_kesulitan_id_foreign` FOREIGN KEY (`kesulitan_id`) REFERENCES `kesulitans` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tasks_sprint_id_foreign` FOREIGN KEY (`sprint_id`) REFERENCES `sprints` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
