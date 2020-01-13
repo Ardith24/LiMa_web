d951d89b65bac114a43a7accbfcf7fd655487090
Ardith Lutfiawan <ardith.lutfiawan@gmail.com>
2020-01-13 08:00:23 +0700

    beneran akhir

--- resources/views/sprint/index.blade.php
+++ resources/views/sprint/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-header border-0">
                     <div class="row align-items-center">
                         <div class="col-8">
-                            <h3 class="mb-0">{{ __('Daftar Sprint') }} - {{ __('3/4') }}</h3>
+                            <h3 class="mb-0">{{ __('Daftar Sprint') }}</h3>
                         </div>
                         <div class="col-4 text-right">
                             <a href="{{ route('sprint.create') }}"
